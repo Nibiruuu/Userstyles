ec83f08e0de89c56481581d857e983812b9f4acc
Tinkei Chen <1759791354@qq.com>
2017-07-19 08:11:38 +0800

    Create Anti-Moe Pedia.js

---
+++ Anti-Moe Pedia/Anti-Moe Pedia.js
@@ -0,0 +1,7 @@
+$("#p-personal ul").append('<li id="pt-talk"><a href="/Talk:%E8%AE%A8%E8%AE%BA%E7%89%88">讨论版</a></li><li id="pt-recent-changes"><a href="/Special:%E6%9C%80%E8%BF%91%E6%9B%B4%E6%94%B9">最近更改</a></li><li id="pt-special"><a href="/Special:%E7%89%B9%E6%AE%8A%E9%A1%B5%E9%9D%A2">特殊页面</a></li>');
+ 
+var str = window.location.href;
+str=str.substring(str.indexOf(".org/") + 5);
+ 
+$("#footer-info-lastmod").wrap('<a href="/index.php?title='+str+'&action=history"></a>');
+$("#firstHeading").append('<span class="mw-editsection">[<a href="/index.php?title='+str+'&action=edit">编辑</a>]</span>');
